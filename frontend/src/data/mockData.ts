2c053f0dcfcac762784806ca4ba976db786a4651
Erick Turriago <turriago-erick@hotmail.com>
2025-06-19 20:16:38 -0500

    Update mock

---
+++ frontend/src/data/mockData.ts
@@ -0,0 +1,5 @@
+export const heroImages = [
+  { id: 1, img: 'https://i.ibb.co/b3VLfF4/C3.png', title: 'Crea Diseños Únicos', subtitle: 'Nuestra herramienta de personalización te da el control total para crear la camiseta perfecta.', buttonText: '¡Personaliza Ahora!', link: '/customize' },
+  { id: 2, img: 'https://i.ibb.co/yqcmkRj/C1-1.png', title: 'Nueva Colección Abstracta', subtitle: 'Explora nuestros diseños más recientes con un toque moderno y minimalista.', buttonText: 'Ver Colección', link: '/tshirts' },
+  { id: 3, img: 'https://i.ibb.co/M5xJchw/C2.png', title: 'Envío Gratis Este Mes', subtitle: 'Aprovecha nuestra promoción de envío gratuito en todas las órdenes superiores a $100.000.', buttonText: 'Comprar Ya', link: '/catalog' }
+];
